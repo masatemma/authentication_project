a35d0fcf7d8a31a896e814c54f7975ef9b3b7810
Masaharu Temma <temma.masa@gmail.com>
2022-02-13 19:10:55 +1100

    Add Environment vars

---
+++ app.js
@@ -0,0 +1,79 @@
+
+require('dotenv').config()
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+const app = express();
+
+
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+mongoose.connect("mongodb://localhost:27017/userDB");
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+});
+
+const secret = process.env.SECRET;
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]});
+
+const User = mongoose.model("User", userSchema);
+
+app.get("/", function(req, res){
+  res.render("home");
+});
+
+//resgister route
+app.route("/register")
+
+.get(function(req, res){
+  res.render("register");
+})
+
+.post(function(req, res){
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
+  });
+
+  newUser.save(function(err){
+    if(err){
+      console.log(err);
+    } else {
+      res.render("secrets");
+    }
+  });
+});
+
+//login route
+app.route("/login")
+
+.get(function(req, res){
+  res.render("login");
+})
+
+.post(function(req, res){
+  const username = req.body.username;
+  const password = req.body.password;
+  User.findOne({emai: username}, function(err, foundUser){
+    if(err){
+      console.log(err);
+    } else if (foundUser) {
+      if (foundUser.password === password) {
+        res.render("secrets");
+      }
+    }
+
+  });
+});
+
+
+
+app.listen(3000, function(){
+  console.log("Server started on port 3000.");
+})
